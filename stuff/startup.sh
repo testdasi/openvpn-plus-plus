c0e5ad265d342b69795452f987b4669670ae97a8
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 00:43:39 +0100

    List all vpn in openvpn_files folder at boot

--- stuff/startup.sh
+++ stuff/startup.sh
@@ -4,6 +4,7 @@
 src='/config/openvpn_files'
 des='/config/openvpn'
 mkdir -p $src
+ls $src/*.ovpn | sort -R
 ls $src/*.ovpn | sort -R | tail -1 | while read file; do
     cp --force "$file" "$des/openvpn.ovpn"
 done
